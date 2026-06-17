44368f9f1c255219db5f5bbb7fa665271858dfa6
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-17 15:38:46 +0300

    Файл конфигурации версии модуля

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии модуля.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'Sign in to Control Panel',
+    'description' => 'Sign in to the website Control Panel',
+    'version'     => '1.0',
+    'versionDate' => '01-07-2015',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-signin/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
