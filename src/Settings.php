3e074617f85be6521da17d0cd8d072ad72db2648
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-17 15:37:50 +0300

    Settings

---
+++ src/Settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\Signin;
+
+use Ge\Config\Config;
+
+/**
+ * Модуль авторизации пользователя.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\Signin
+ * @since 1.0
+ */
+class Settings extends Config
+{
+}
