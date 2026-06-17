de0dba59195778d2cdd921ca862d0f7e3e6942de
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-17 15:38:34 +0300

    Файл конфигурации настройки модуля

---
+++ config/.settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации настройки модуля.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'maxAttempts' => 3 // количество попыток авторизации
+];
